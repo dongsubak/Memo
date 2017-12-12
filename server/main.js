df8b945dd3b25b25927828c45e98524af51da175
Dongsu Park <blackxub@naver.com>
2017-12-12 22:47:06 +0900

    add modules

--- server/main.js
+++ server/main.js
@@ -4,11 +4,30 @@ import path from 'path';
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 
+import morgan from 'morgan'; // HTTP REQUEST LOGGER
+import bodyParser from 'body-parser'; // PARSE HTML BODY
+
+import mongoose from 'mongoose';
+import session from 'express-session';
+
 const app = express();
 const port = 3000;
 const devPort = 4000;
+const db = mongoose.connection;
 
+db.on('error', console.error);
+db.once('open', () => { console.log('Connected to mongodb server'); });
+mongoose.connect('mongodb://username:password@host:port/database=');
+
+app.use(session({
+    secret: '',
+    resave: false,
+    saveUninitialized: true
+}));
 app.use('/', express.static(path.join(__dirname, './../public')));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => {
     return res.send('Hello CodeLab');
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,18 +1,18 @@
 var webpack = require('webpack');
- 
+
 module.exports = {
- 
+
     entry: [
         './src/index.js',
         'webpack-dev-server/client?http://0.0.0.0:4000',
         'webpack/hot/only-dev-server'
     ],
- 
+
     output: {
         path: '/',
         filename: 'bundle.js'
     },
- 
+
     devServer: {
         hot: true,
         filename: 'bundle.js',
@@ -31,23 +31,25 @@ module.exports = {
           timings: false,
           chunks: false,
           chunkModules: false
-        } 
+        }
     },
 
-    
+
     plugins: [
         new webpack.HotModuleReplacementPlugin()
     ],
- 
+
     module: {
         loaders: [
             {
-                test: /\.js$/,
-                loaders: ['react-hot', 'babel?' + JSON.stringify({
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react']
-                })],
+                test: /.js$/,
+                loader: 'babel',
                 exclude: /node_modules/,
+                query: {
+                  cacheDirectory: true,
+                  presets: ['es2015', 'react'],
+                  plugins: ["react-hot-loader/babel"]
+                }
             }
         ]
     }
